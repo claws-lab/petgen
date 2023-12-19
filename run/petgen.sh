5e3ebe8bba6ae71f99002fd99b5d1b4157aa7d3c
binghesam <bing_he@foxmail.com>
2023-12-19 17:32:12 -0500

    fix typo

--- run/petgen.sh
+++ run/petgen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # The first 0 is job_id, the second 0 is gpu_id
-# PET is based on ICLR'19 RelGAN.
+# PETGEN is based on ICLR'19 RelGAN.
 # we modify the relgan_instructor.py file and call the new revised class.
 python run_relgan.py 0 0
